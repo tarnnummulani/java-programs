94cc17618a793e59f46c4680daf0d52898244213
tarnnummulani <tarnnummulani@gmail.com>
2019-03-15 08:57:16 +0530

    blank

---
+++ NthFibonacciNo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class NthFibonacciNo
+{
+	static int fib(int n){
+		if(n<=1)
+			return n;
+		return fib(n-1)+fib(n-2);
+	}
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the no:");
+		int n=sc.nextInt();
+		System.out.println(fib(n));
+	}
+}
